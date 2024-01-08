d96f368552988b5e262b7ce239957527f05a335b
Mustafa Nural <mvnural@gmail.com>
2024-01-08 11:44:43 -0500

    Remove ddl for the version schemas, as they are no longer installed by default.

--- Main/lib/sql/apidbschema/Postgres/restoreNullConstraintsFromSimilarityTables.sql
+++ Main/lib/sql/apidbschema/Postgres/restoreNullConstraintsFromSimilarityTables.sql
@@ -12,17 +12,3 @@ ALTER TABLE DoTS.SimilaritySpan ALTER COLUMN number_positive SET NOT NULL;
 ALTER TABLE DoTS.SimilaritySpan ALTER COLUMN score SET NOT NULL;
 ALTER TABLE DoTS.SimilaritySpan ALTER COLUMN pvalue_mant SET NOT NULL;
 ALTER TABLE DoTS.SimilaritySpan ALTER COLUMN pvalue_exp SET NOT NULL;
-
-ALTER TABLE DoTSVer.SimilarityVer ALTER COLUMN total_match_length SET NOT NULL;
-ALTER TABLE DoTSVer.SimilarityVer ALTER COLUMN number_identical SET NOT NULL;
-ALTER TABLE DoTSVer.SimilarityVer ALTER COLUMN number_positive SET NOT NULL;
-ALTER TABLE DoTSVer.SimilarityVer ALTER COLUMN score SET NOT NULL;
-ALTER TABLE DoTSVer.SimilarityVer ALTER COLUMN pvalue_mant SET NOT NULL;
-ALTER TABLE DoTSVer.SimilarityVer ALTER COLUMN pvalue_exp SET NOT NULL;
-
-ALTER TABLE DoTSVer.SimilaritySpanVer ALTER COLUMN match_length SET NOT NULL;
-ALTER TABLE DoTSVer.SimilaritySpanVer ALTER COLUMN number_identical SET NOT NULL;
-ALTER TABLE DoTSVer.SimilaritySpanVer ALTER COLUMN number_positive SET NOT NULL;
-ALTER TABLE DoTSVer.SimilaritySpanVer ALTER COLUMN score SET NOT NULL;
-ALTER TABLE DoTSVer.SimilaritySpanVer ALTER COLUMN pvalue_mant SET NOT NULL;
-ALTER TABLE DoTSVer.SimilaritySpanVer ALTER COLUMN pvalue_exp SET NOT NULL;
