c9690d3aab6047702cad046c54c2f0225daadda0
Mustafa Nural <mvnural@gmail.com>
2024-05-13 11:37:13 -0400

    Partially Revert: Do not create obsolete tuning tables at install time.
    StudyIdDatasetId is managed by tuning manager but eda.StudyIdDatasetId view depends on this. Therefore we need to create it during install.

--- Main/lib/sql/apidbschema/Postgres/createApidbTuningSchema.sql
+++ Main/lib/sql/apidbschema/Postgres/createApidbTuningSchema.sql
@@ -37,3 +37,15 @@ SELECT NEXTVAL('core.databaseinfo_sq'), 'ApidbTuning',
        1, 1, 1, 1, 1, 1, 1, 1, p.project_id, 0
 FROM (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p
 WHERE lower('ApidbTuning') NOT IN (SELECT lower(name) FROM core.DatabaseInfo);
+
+
+CREATE TABLE apidbTuning.StudyIdDatasetId0000 (
+  study_stable_id varchar(200),
+  dataset_id      varchar(15)
+);
+
+-- Posgtres doesn't have synonyms. Convert them to views instead
+-- create or replace synonym apidbTuning.StudyIdDatasetId for apidbTuning.StudyIdDatasetId0000;
+CREATE OR REPLACE VIEW apidbtuning.studyiddatasetid AS SELECT * FROM apidbtuning.studyiddatasetid0000;
+
+-----------------------------------
