27ae0fbc7140c1d4cebc6bb2473b20a2b37cee0b
sybah2 <saikouybah@gmail.com>
2025-02-12 10:45:29 -0500

    Spell correction in createAntiSmashTable.sql

--- Main/lib/sql/apidbschema/Postgres/createAntiSmashTable.sql
+++ Main/lib/sql/apidbschema/Postgres/createAntiSmashTable.sql
@@ -1,5 +1,5 @@
 ----- Create a table for the antiSMASH clusters -----------------------
-CREATE TABLE ApiDB.antiSmashClutser (
+CREATE TABLE ApiDB.antiSmashCluster (
    internal_id                   NUMERIC(10),
    cluster_start                 NUMERIC(10),
    cluster_end                   NUMERIC(10),
@@ -19,11 +19,11 @@ CREATE TABLE ApiDB.antiSmashClutser (
  );
  
  
- CREATE SEQUENCE ApiDB.antiSmashClutser_sq;
+ CREATE SEQUENCE ApiDB.antiSmashCluster_sq;
  
- GRANT INSERT, SELECT, UPDATE, DELETE ON ApiDB.antiSmashClutser TO gus_w;
- GRANT SELECT ON ApiDB.antiSmashClutser TO gus_r;
- GRANT SELECT ON ApiDB.antiSmashClutser_sq TO gus_w;
+ GRANT INSERT, SELECT, UPDATE, DELETE ON ApiDB.antiSmashCluster TO gus_w;
+ GRANT SELECT ON ApiDB.antiSmashCluster TO gus_r;
+ GRANT SELECT ON ApiDB.antiSmashCluster_sq TO gus_w;
  
  INSERT INTO core.TableInfo
    (table_id, name, table_type, primary_key_column, database_id,
@@ -31,12 +31,12 @@ CREATE TABLE ApiDB.antiSmashClutser (
      modification_date, user_read, user_write, group_read, group_write,
      other_read, other_write, row_user_id, row_group_id, row_project_id,
      row_alg_invocation_id)
-   SELECT NEXTVAL('core.tableinfo_sq'), 'antiSmashClutser', 'Standard', 'antiSmashClutser_id',
+   SELECT NEXTVAL('core.tableinfo_sq'), 'antiSmashCluster', 'Standard', 'antiSmashCluster_id',
      d.database_id, 0, 0, NULL, NULL, 1, localtimestamp, 1, 1, 1, 1, 1, 1, 1, 1, p.project_id, 0
    FROM
         (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
         (SELECT database_id FROM core.DatabaseInfo WHERE name = 'ApiDB') d
-   WHERE 'antiSmashClutser' NOT IN (SELECT name FROM core.TableInfo
+   WHERE 'antiSmashCluster' NOT IN (SELECT name FROM core.TableInfo
    WHERE database_id = d.database_id);
 
 ------------------------------------------------------------------
