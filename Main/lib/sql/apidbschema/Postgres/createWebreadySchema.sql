760948827c3cef56432b6c10e90f0da07f81dec0
Mustafa Nural <mvnural@gmail.com>
2025-05-29 09:54:15 -0400

    Add webready schema to the installer.

---
+++ Main/lib/sql/apidbschema/Postgres/createWebreadySchema.sql
@@ -0,0 +1,14 @@
+CREATE SCHEMA webready;
+
+GRANT USAGE ON SCHEMA webready TO gus_r;
+GRANT USAGE, CREATE ON SCHEMA webready TO gus_w;
+
+INSERT INTO core.DatabaseInfo
+   (database_id, name, description, modification_date, user_read, user_write,
+    group_read, group_write, other_read, other_write, row_user_id,
+    row_group_id, row_project_id, row_alg_invocation_id)
+SELECT NEXTVAL('core.databaseinfo_sq'), 'Webready',
+       'Denormalized webready tables for the websites created by the workflow', localtimestamp,
+       1, 1, 1, 1, 1, 1, 1, 1, p.project_id, 0
+FROM (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p
+WHERE lower('webready') NOT IN (SELECT lower(name) FROM core.DatabaseInfo);
