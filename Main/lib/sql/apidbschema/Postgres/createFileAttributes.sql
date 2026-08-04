aa9fbf24e2907f678d1a6a4849551cb3e907ef41
Mustafa Nural <mvnural@gmail.com>
2026-08-04 16:01:37 -0400

    drop core.tableinfo
    - we don't need the row in core.table info since this isn't being loaded as a standard gus plugin. You can remove that from both sql files

--- Main/lib/sql/apidbschema/Postgres/createFileAttributes.sql
+++ Main/lib/sql/apidbschema/Postgres/createFileAttributes.sql
@@ -14,20 +14,3 @@ CREATE TABLE apidb.FileAttributes (
 
 GRANT select ON apidb.FileAttributes TO gus_r;
 GRANT insert, update, delete ON apidb.FileAttributes TO gus_w;
-
-
-INSERT INTO core.TableInfo
-    (table_id, name, table_type, primary_key_column, database_id, is_versioned,
-     is_view, view_on_table_id, superclass_table_id, is_updatable,
-     modification_date, user_read, user_write, group_read, group_write,
-     other_read, other_write, row_user_id, row_group_id, row_project_id,
-     row_alg_invocation_id)
-SELECT NEXTVAL('core.tableinfo_sq'), 'FileAttributes',
-       'Standard', 'file_id',
-       d.database_id, 0, 0, NULL, NULL, 1, localtimestamp, 1, 1, 1, 1, 1, 1, 1, 1,
-       p.project_id, 0
-FROM
-     (SELECT MAX(project_id) AS project_id FROM core.ProjectInfo) p,
-     (SELECT database_id FROM core.DatabaseInfo WHERE lower(name) = 'apidb') d
-WHERE 'fileattributes' NOT IN (SELECT lower(name) FROM core.TableInfo
-                                    WHERE database_id = d.database_id);
--- Main/lib/sql/apidbschema/Postgres/dropFileAttributes.sql
+++ Main/lib/sql/apidbschema/Postgres/dropFileAttributes.sql
@@ -1,7 +1,1 @@
 DROP TABLE apidb.FileAttributes;
-
-DELETE FROM core.TableInfo
-WHERE lower(name) = lower('FileAttributes')
-  AND database_id = (SELECT database_id
-                     FROM core.DatabaseInfo
-                     WHERE lower(name) = 'apidb');
